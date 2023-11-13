4f2fc5077ce19219a8aeebbb2ea893abd039df57
user <user@smhrd>
2023-11-13 13:06:19 +0900

    1113_13:06_김지홍_ 위치스 인턴과정 프로젝트 생성

---
+++ witches/src/main/java/com/witches/booking/WitchesApplication.java
@@ -0,0 +1,13 @@
+package com.witches.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WitchesApplication.class, args);
+	}
+
+}
---
+++ witches/src/test/java/com/witches/booking/WitchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.witches.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
