420f98ef1e0828020a9840d16ee3b245341a8efc
wjddb <wjddb@yooonjiii>
2023-11-17 11:41:59 +0900

    윤지 20231117 권한작업중

--- witches/src/main/java/com/witches/schedule/mapper/ScheduleMapper.java
+++ witches/src/main/java/com/witches/schedule/mapper/ScheduleMapper.java
@@ -14,9 +14,9 @@ public interface ScheduleMapper {
 	@Select("select * from schedule")
 	public List<ScheduleVO> showSchedule();
 
-	@Select("SELECT id, year, month, day, DATE_FORMAT(start, '%H:%i') as start, DATE_FORMAT(end, '%H:%i') as end, people_num, name, department, contents, is_delete, cancle_reason, cancle_name,email FROM schedule WHERE month=#{month} AND is_delete='N'")
+	@Select("SELECT id, year, month, day, DATE_FORMAT(start, '%H:%i') as start, DATE_FORMAT(end, '%H:%i') as end, people_num, name, department, contents, is_delete, cancle_reason, cancle_name, create_nm FROM schedule WHERE month=#{month} AND is_delete='N'")
 	public List<ScheduleVO> showScheduleList(int month);	
 	
-	@Select("SELECT id, year, month, day, DATE_FORMAT(start, '%H:%i') as start, DATE_FORMAT(end, '%H:%i') as end, people_num, name, department, contents, is_delete, cancle_reason, cancle_name,email FROM schedule WHERE is_delete='N'")
+	@Select("SELECT id, year, month, day, DATE_FORMAT(start, '%H:%i') as start, DATE_FORMAT(end, '%H:%i') as end, people_num, name, department, contents, is_delete, cancle_reason, cancle_name, create_nm FROM schedule WHERE is_delete='N'")
 	public List<ScheduleVO> showSchduleAll();
 }
