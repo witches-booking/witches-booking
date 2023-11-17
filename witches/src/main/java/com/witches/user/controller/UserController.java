b02cb10a214f88855e0786304450ebd30b4b17d6
philosokey-M <wlghd99589@gmail.com>
2023-11-17 15:11:49 +0900

    1117_15:31_김지홍_일단 커밋

--- witches/src/main/java/com/witches/user/controller/UserController.java
+++ witches/src/main/java/com/witches/user/controller/UserController.java
@@ -34,28 +34,27 @@ import reactor.core.publisher.Mono;
 @CrossOrigin(origins = "*")
 @RestController
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	/**
 	 * 로그인
-	 * @param UserVO userVo
-	 * @param String loginId
-	 * @param String sns
+	 * 
+	 * @param UserVO   userVo
+	 * @param String   loginId
+	 * @param String   sns
 	 * @param ResultVO resultVo
-	 * @return Gson gson
-	 * ------------ 이력 ------------
-	 * 2023.11.17 / 정윤지 / 최초 적용
+	 * @return Gson gson ------------ 이력 ------------ 2023.11.17 / 정윤지 / 최초 적용
 	 */
 	@PostMapping("/login")
-	public ResponseEntity<Object> login(@ModelAttribute UserVO userVo, Model model
-			, @RequestParam String loginId, @RequestParam String sns) {
+	public ResponseEntity<Object> login(@ModelAttribute UserVO userVo, Model model, @RequestParam String loginId,
+			@RequestParam String sns) {
 
 		userVo.setLoginId(loginId);
 		userVo.setSns(sns);
-		System.out.println("로그인 id:::::::::"+userVo.getLoginId());
-		System.out.println("로그인 sns:::::::::"+userVo.getSns());
+		System.out.println("로그인 id:::::::::" + userVo.getLoginId());
+		System.out.println("로그인 sns:::::::::" + userVo.getSns());
 		model.addAttribute("loginId", userVo.getLoginId());
 		Gson gson = new GsonBuilder().create();
 		ResultVO resultVo = userService.login(userVo);
@@ -64,61 +63,40 @@ public class UserController {
 
 	// 카카오 로그인 restApi
 	@RequestMapping("/kakaoLogin")
-	public Mono<String> kakaoLogin(@RequestParam String code,HttpSession session ) {
-
-		System.out.println("rest카카오로그인 컨트롤러 실행");
+	public String kakaoLogin(@RequestParam String code, HttpSession session) {
+	    System.out.println("rest카카오로그인 컨트롤러 실행");
 	    String restApiKey = "02b86e71e0895cda12a9361c1cdb773a";
 	    String redirectUri = "http://localhost:8449/kakaoLogin";
-
+	    System.out.println("code 값 확인: "+code);
 	    // WebClient 인스턴스 생성
 	    WebClient webClient = WebClient.create();
 
 	    // 토큰 받아오기
-	    String tokenUrl = "https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=" + restApiKey + "&redirect_uri=" + redirectUri + "&code=" + code;
-	    Mono<String> tokenResponse = webClient.get()
-	            .uri(tokenUrl)
-	            .retrieve()
-	            .bodyToMono(String.class);
+	    String tokenUrl = "https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=" + restApiKey
+	            + "&redirect_uri=" + redirectUri + "&code=" + code;
+	    String tokenResponse = webClient.get().uri(tokenUrl).retrieve().bodyToMono(String.class).block();
 
-	    return tokenResponse.flatMap(response -> {
-	        // JSON 데이터 파싱
-	        JsonParser jsonParser = new JsonParser();
-	        JsonObject jsonObject = jsonParser.parse(response).getAsJsonObject();
+	    // JSON 데이터 파싱
+	    JsonParser jsonParser = new JsonParser();
+	    JsonObject jsonObject = jsonParser.parse(tokenResponse).getAsJsonObject();
 
-	        String accessToken = jsonObject.get("access_token").getAsString();
+	    String accessToken = jsonObject.get("access_token").getAsString();
+	    System.out.println("엑세스 토큰 값 확인 ;"+ accessToken);
+	    // 사용자 정보 받아오기
+	    String userUrl = "https://kapi.kakao.com/v2/user/me";
+	    String userResponse = webClient.get().uri(userUrl)
+	            .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken).retrieve().bodyToMono(String.class).block();
 
-	        // 사용자 정보 받아오기
-	        String userUrl = "https://kapi.kakao.com/v2/user/me";
-	        Mono<String> userResponse = webClient.get()
-	                .uri(userUrl)
-	                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
-	                .retrieve()
-	                .bodyToMono(String.class);
+	    // JSON 데이터 파싱
+	    JsonObject userJson = jsonParser.parse(userResponse).getAsJsonObject();
+	    String id = userJson.get("id").getAsString();
+	    System.out.println("아이디값 확인 : "+ id);
+	    session.setAttribute("createNm", id);
 
-	        return userResponse.map(userResponseStr -> {
-	            // JSON 데이터 파싱
-	            JsonObject userJson = jsonParser.parse(userResponseStr).getAsJsonObject();
+	    // TODO: DB에서 사용자 데이터 확인 및 저장하는 코드를 추가하세요.
 
-	            String id = userJson.get("id").getAsString();
-	            
-	            session.setAttribute("createNm", id);
+	    // JSP 페이지 이름 반환
+	    return "redirect:/Calendar"; // 여기에 원하는 JSP 페이지 이름을 입력하세요.
+	} // kakaoLogin함수 끝
 
-	            // TODO: DB에서 사용자 데이터 확인 및 저장하는 코드를 추가하세요.
-	            // ModelAndView 객체 생성
-	            ModelAndView mav = new ModelAndView();
-	            
-	            // JSP 페이지 이름 설정
-	            mav.setViewName("/"); // 여기에 원하는 JSP 페이지 이름을 입력하세요.
-	            
-	            // 토큰 값과 ID 값을 반환
-	            return "Calendar.jsp";
-	        });
-	    });
-	
-	
-	} //kakaoLogin함수 끝
-	
-	
-
-	
 }
