f2dc3ac0471a8aabcde1c1636617c2561376cb04
philosokey-M <wlghd99589@gmail.com>
2023-11-20 14:34:03 +0900

    1120_14:33_김지홍_카카오로그인 주소 변경

--- witches/src/main/java/com/witches/schedule/mapper/ScheduleMapper.java
+++ witches/src/main/java/com/witches/schedule/mapper/ScheduleMapper.java
@@ -16,7 +16,7 @@ public interface ScheduleMapper {
 	@Select("select * from schedule")
 	public List<ScheduleVO> showSchedule();
 
-	@Select("SELECT id, year, month, day, DATE_FORMAT(start, '%H:%i') as start, DATE_FORMAT(end, '%H:%i') as end, people_num, name, department, contents, is_delete, cancle_reason, cancle_name, create_nm FROM schedule WHERE month=#{month} AND is_delete='N'")
+	@Select("SELECT id, year, month, day, DATE_FORMAT(start, '%H:%i') as start, DATE_FORMAT(end, '%H:%i') as end, people_num, name, department, contents, is_delete, cancle_reason, cancle_name, create_nm FROM schedule WHERE month=#{month} AND is_delete='N' ORDER BY start ASC")
 	public List<ScheduleVO> showScheduleList(int month);	
 	
 	@Select("SELECT id, year, month, day, DATE_FORMAT(start, '%H:%i') as start, DATE_FORMAT(end, '%H:%i') as end, people_num, name, department, contents, is_delete, cancle_reason, cancle_name, create_nm FROM schedule WHERE is_delete='N'")
